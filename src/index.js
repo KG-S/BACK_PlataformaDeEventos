e1adc238309359270ab97b41b83c992d8f6a2b5d
KG-S <k.gabriel@aluno.ifsp.edu.br>
2025-11-11 23:22:54 -0300

    fix: changing to new routes

--- src/index.js
+++ src/index.js
@@ -8,18 +8,25 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 
-// Define endereço do swagger
+
 app.use(
   "/api",
   swaggerUi.serve,
   swaggerUi.setup(specs, {
-    customCss: ".swagger-ui .topbar { display: none }", // Remove a barra de identificação do swagger.
+    customCss: ".swagger-ui .topbar { display: none }", 
     customSiteTitle: "Documentação da API de exemplo",
   })
 );
 
-const routes = require("./routes/rota");
-app.use("/", routes);
+const organizadoresRoutes = require("./routes/organizadores");
+const eventosRoutes = require("./routes/eventos");
+const participantesRoutes = require("./routes/participantes");
+const registroRoutes = require("./routes/registros");
+
+app.use("/organizadores", organizadoresRoutes);
+app.use("/eventos", eventosRoutes);
+app.use("/participantes", participantesRoutes);
+app.use("/registros", registroRoutes);
 
 app.listen(port, () => {
   console.log(`Servidor executando em http://localhost:${port}`);
